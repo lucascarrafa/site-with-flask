21143a98b48bea58e79ab55779492f299a58d80e
Lucas Carrafa <lucascarrafa@hotmail.com>
2018-12-17 15:55:04 -0200

    start

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config.from_object('config')
+manager = Manager(app)
+
+from app.controllers import default
+
